f5da378dd2af19b53562e587404a3a187add9ca8
nikolaaleksic1994 <aleksicn99@gmail.com>
2021-05-21 18:08:27 +0200

    first comit

---
+++ cypress/integration/examples/login.spec.js
@@ -0,0 +1,10 @@
+describe('login spec', () =>{
+    it('visit gallery app', () => {
+
+        cy.visit('https://gallery-app.vivifyideas.com/')
+    })
+
+    it('click login button', () => {
+       cy.get('a[class="nav-link nav-buttons"]').eq(0).click()
+    })
+})
